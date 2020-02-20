7e2f8ed91841cc0a3f7cb08edb574c17013982b3
Jumpyvonvagabond <36978560+Jumpyvonvagabond@users.noreply.github.com>
2020-02-19 16:08:44 -0800

    ported to cogs

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,30 +1,63 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import bot
 from discord.ext.commands import MemberConverter
-from discord.utils import get
 import json
 with open('config.json') as f:
   data = json.loads(f.read())
 bot = commands.Bot(command_prefix=data["prefix"])
 
-async def mention(ctx):
-  role = ctx.message.content[len(data["prefix"]) + len(ctx.invoked_with) + 1:]
-  try:
-    role = get(ctx.guild.roles, name = role)
-  except:
-    await ctx.channel.send("Role not found")
-  else:
-    await ctx.channel.send("What channel would you like the mention in?")
-    def check(m):
-      return m.author == ctx.message.author and m.channel == ctx.message.channel
-    msg = await bot.wait_for('message', check=check, timeout=30)
-    await ctx.channel.send(msg.content)
+class Utility(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self._last_member = None
 
-    #try:
-      #channel = get(ctx.guild.channels, name = sendin)
-      #await role.edit(mentionable = True, reason = f"Called on by: {ctx.message.author}")
-      #await ctx.channel.send(role.mention)
-      #await role.edit(mentionable = False, reason = f"Called on by: {ctx.message.author}")
-    #except:
-      #await ctx.channel.send("Missing Permissions")
+  @commands.command()
+  async def mention(self, ctx):
+    role = ctx.message.content[len(data["prefix"]) + len(ctx.invoked_with) + 1:]
+    try:
+      role = get(ctx.guild.roles, name = role)
+    except:
+      await ctx.channel.send("Role not found")
+    else:
+      try:
+        await role.edit(mentionable = True, reason = f"Called on by: {ctx.message.author}")
+        await ctx.channel.send(role.mention)
+        await role.edit(mentionable = False, reason = f"Called on by: {ctx.message.author}")
+      except:
+        await ctx.channel.send("Missing Permissions")
+  @commands.command()
+  async def actionlist(self, ctx, event, member = None):
+    if ctx.message.author.guild_permissions.manage_messages == False: 
+      await ctx.channel.send("You don't have permission to use this command)")
+    else:
+      if member == None:
+        embed = discord.Embed(title = f'List of {event.title()}s', description = None, colour=discord.Colour.blue())
+        action = getattr(discord.AuditLogAction,event)
+        async for entry in ctx.guild.audit_logs(limit = 15, action=action):
+          embed.add_field(name = '{0.user}'.format(entry), value = f'{event.title()}'+' to {0.target} Entry ID: {0.id}'.format(entry), inline = True)
+        await ctx.channel.send(embed=embed)
+      else:
+        try:
+          member = await MemberConverter().convert(ctx, member)
+        except:
+          print("\nError in command: Ban:\nUser not found, or no user was specified")
+        embed = discord.Embed(title = f'Action list for {member.mention}', description = None, colour=discord.Colour.blue())
+        action = getattr(discord.AuditLogAction,event)
+        async for entry in ctx.guild.audit_logs(limit = 15, action=action, user=member):
+          embed.add_field(name = '{0.user}'.format(entry), value = f'{event.title()}'+' to {0.target} Entry ID: {0.id}'.format(entry), inline = True)
+        await ctx.channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_member_join(self, member):
+    general = self.bot.get_channel(int(data["generalchannel"]))
+    if general is None:
+      print("the command didn't work LOL YOU SUCK")
+    welcomemessage = f"Welcome, {member.mention}! Come join us in {general.mention}"
+    embed = discord.Embed(title = "Welcome", description = welcomemessage, colour=discord.Colour.blue())
+    embed.set_footer(text="We're glad to have you")
+    embed.set_image(url=data["welcomeimage"])
+    channel = self.bot.get_channel(int(data["welcomechannel"]))
+    await channel.send(embed=embed)
+
+def setup(bot):
+  bot.add_cog(Utility(bot))
