ceb8d6fc3c533daf714ff67c62bf37beda3a3e43
Jumpyvonvagabond <36978560+Jumpyvonvagabond@users.noreply.github.com>
2020-05-01 21:57:35 -0700

    added permission checks

--- cogs/settings.py
+++ cogs/settings.py
@@ -58,145 +58,156 @@ class Settings(commands.Cog):
         pages = 1
       embed.set_footer(text=f'Page 1 of {pages}')
       await ctx.send(embed=embed)
-    try:
-      member = await MemberConverter().convert(ctx, owner)
-      cfg.data['ownerid'].append(member.id)
-      dump()
-      await listowners(ctx, owner)
-    except:
-      await listowners(ctx, owner)
+    if any(identification in str(ctx.message.author.id) for identification in str(cfg.data["ownerid"])):
+      try:
+        member = await MemberConverter().convert(ctx, owner)
+        cfg.data['ownerid'].append(member.id)
+        dump()
+        await listowners(ctx, owner)
+      except:
+        await listowners(ctx, owner)
+    else:
+      listowners(ctx, owner)
     
   @commands.command()
   async def welcome(self, ctx, *, message = None):
-    if message == None:
-      embed = cfg.buildembed("Welcome Info", f"Here is a list of options, and their current values for {ctx.guild.name}")
-      embed.add_field(name="Welcome message", value=f"To make a custom welcome message, use '{ctx.prefix}welcome message [message]'\nTo ping the user who joined, say {{MENTION}} where you want them to be pinged", inline=False)
-      embed.add_field(name="Channel", value=f"To change the welcome channel, use '{ctx.prefix}welcome channel [channel]'", inline=False)
-      embed.add_field(name="Toggle", value=f"To toggle the welcome message on and off, use '{ctx.prefix}welcome toggle'\n", inline=False)
-      await ctx.channel.send(embed=embed)
+    if ctx.message.author.guild_permissions.manage_channels:
+      if message == None:
+        embed = cfg.buildembed("Welcome Info", f"Here is a list of options, and their current values for {ctx.guild.name}")
+        embed.add_field(name="Welcome message", value=f"To make a custom welcome message, use '{ctx.prefix}welcome message [message]'\nTo ping the user who joined, say {{MENTION}} where you want them to be pinged", inline=False)
+        embed.add_field(name="Channel", value=f"To change the welcome channel, use '{ctx.prefix}welcome channel [channel]'", inline=False)
+        embed.add_field(name="Toggle", value=f"To toggle the welcome message on and off, use '{ctx.prefix}welcome toggle'\n", inline=False)
+        await ctx.channel.send(embed=embed)
+      else:
+        message = message.split(" ", 1)
+        if message[0].lower() == "message":
+          if len(message) == 1:
+            await ctx.message.send("You forgot to input a new welcome message!")
+          else:
+            cfg.data['welcome']['message'] = message[1]
+            dump()
+        elif message[0].lower() == "channel":
+          try:
+            channel = await TextChannelConverter().convert(ctx, (message[1]))
+          except:
+            await ctx.send("Invalid channel input")
+          else:
+            cfg.data['welcome']['channel'] = channel.id
+            dump()
+        elif message[0].lower() == "toggle":
+          if cfg.data['welcome']['enabled'] == False:
+            cfg.data['welcome']['enabled'] = True
+            dump()
+            self.bot.reload_extension('cogs.utility')
+          else:
+            cfg.data['welcome']['enabled'] = False
+            dump()
+            cog = self.bot.get_cog('Utility')
+            self.bot.remove_listener(cog.on_member_join)
     else:
-      message = message.split(" ", 1)
-      if message[0].lower() == "message":
-        if len(message) == 1:
-          await ctx.message.send("You forgot to input a new welcome message!")
-        else:
-          cfg.data['welcome']['message'] = message[1]
-          dump()
-      elif message[0].lower() == "channel":
-        try:
-          channel = await TextChannelConverter().convert(ctx, (message[1]))
-        except:
-          await ctx.send("Invalid channel input")
-        else:
-          cfg.data['welcome']['channel'] = channel.id
-          dump()
-      elif message[0].lower() == "toggle":
-        if cfg.data['welcome']['enabled'] == False:
-          cfg.data['welcome']['enabled'] = True
-          dump()
-          self.bot.reload_extension('cogs.utility')
-        else:
-          cfg.data['welcome']['enabled'] = False
-          dump()
-          cog = self.bot.get_cog('Utility')
-          self.bot.remove_listener(cog.on_member_join)
+      await ctx.send("This command requires the manage channels permission")
   
   @commands.command()
   async def wordfilter(self, ctx, *, word = None):
-    if word == None:
-      embed = cfg.buildembed("Word Filter Info", "Here is a list of word filter settings")
-      embed.add_field(name="Toggle", value=f"To toggle the word filter on and off, use '{ctx.prefix}wordfilter toggle'", inline=False)
-      embed.add_field(name="Add", value=f"To add a word to the filter, use '{ctx.prefix}wordfilter add [word]\nYou can use a phrase in place of a word'", inline=False)
-      await ctx.send(embed=embed)
-    elif word.lower() == 'toggle':
-      cog = self.bot.get_cog('Administration')
-      if cfg.data['wordfilter'] == False:
-        cfg.data['wordfilter'] = True
-        self.bot.reload_extension('cogs.administration')
-        dump()
+    if ctx.message.author.guild_permissions.manage_channels:
+      if word == None:
+        embed = cfg.buildembed("Word Filter Info", "Here is a list of word filter settings")
+        embed.add_field(name="Toggle", value=f"To toggle the word filter on and off, use '{ctx.prefix}wordfilter toggle'", inline=False)
+        embed.add_field(name="Add", value=f"To add a word to the filter, use '{ctx.prefix}wordfilter add [word]\nYou can use a phrase in place of a word'", inline=False)
+        await ctx.send(embed=embed)
+      elif word.lower() == 'toggle':
+        cog = self.bot.get_cog('Administration')
+        if cfg.data['wordfilter'] == False:
+          cfg.data['wordfilter'] = True
+          self.bot.reload_extension('cogs.administration')
+          dump()
+        else:
+          cfg.data['wordfilter'] = False
+          self.bot.remove_listener(cog.on_message)
+          dump()
       else:
-        cfg.data['wordfilter'] = False
-        self.bot.remove_listener(cog.on_message)
+        word = word.split(' ', 1)
+        cfg.data['slurs'].append(word[1])
         dump()
     else:
-      word = word.split(' ', 1)
-      cfg.data['slurs'].append(word[1])
-      dump()
+      await ctx.send("This command requires the manage channels permission")
   
   @commands.command(aliases=['pb'])
   async def pineappleboard(self, ctx, *, payload = None):
-    if payload == 'toggle':
-      if cfg.data['pineappleboard']['enabled'] == False:
-         cfg.data['pineappleboard']['enabled'] = True
-         self.bot.reload_extension('cogs.utility')
-      else:
-         cfg.data['pineappleboard']['enabled'] = False
-         cog = self.bot.get_cog('Utility')
-         self.bot.remove_listener(cog.on_reaction_add)
-      dump()
-    elif payload.split(" ", 1)[0].lower() == 'count':
-      try:
-        int(payload.split(" ", 1)[1])
-      except:
-        await ctx.send("The count must be a number!")
-      else:
-        cfg.data['pineappleboard']['count'] = int(payload.split(" ", 1)[1])
+    if ctx.message.author.guild_permissions.manage_channels:
+      if payload == 'toggle':
+        if cfg.data['pineappleboard']['enabled'] == False:
+          cfg.data['pineappleboard']['enabled'] = True
+          self.bot.reload_extension('cogs.utility')
+        else:
+          cfg.data['pineappleboard']['enabled'] = False
+          cog = self.bot.get_cog('Utility')
+          self.bot.remove_listener(cog.on_reaction_add)
         dump()
-    elif payload.split(" ", 1)[0].lower() == 'channel':
-      try:
-        payload = await TextChannelConverter().convert(ctx, str(payload.split(" ", 1)[1]))
-      except:
-        await ctx.send("Invalid channel input")
+      elif payload.split(" ", 1)[0].lower() == 'count':
+        try:
+          int(payload.split(" ", 1)[1])
+        except:
+          await ctx.send("The count must be a number!")
+        else:
+          cfg.data['pineappleboard']['count'] = int(payload.split(" ", 1)[1])
+          dump()
+      elif payload.split(" ", 1)[0].lower() == 'channel':
+        try:
+          payload = await TextChannelConverter().convert(ctx, str(payload.split(" ", 1)[1]))
+        except:
+          await ctx.send("Invalid channel input")
+        else:
+          cfg.data['pineappleboard']['channel'] = payload.id
+          dump()
       else:
-        cfg.data['pineappleboard']['channel'] = payload.id
-        dump()
+        embed = cfg.buildembed("Pineappleboard Settings", "Here's a list of settings")
+        embed.add_field(name="Count", value=f"To change the number of pineapples required to get a message added to the board, use {ctx.prefix}pineappleboard count [number]")
+        embed.add_field(name="Channel", value=f"To change the channel that messages added to the board are sent to, use {ctx.prefix}pineappleboard channel [channel]")
+        embed.add_field(name="Toggle", value=f"To toggle the pineappleboard on and off, use {ctx.prefix}pineappleboard toggle")
+        await ctx.send(embed=embed)
     else:
-      embed = cfg.buildembed("Pineappleboard Settings", "Here's a list of settings")
-      embed.add_field(name="Count", value=f"To change the number of pineapples required to get a message added to the board, use {ctx.prefix}pineappleboard count [number]")
-      embed.add_field(name="Channel", value=f"To change the channel that messages added to the board are sent to, use {ctx.prefix}pineappleboard channel [channel]")
-      embed.add_field(name="Toggle", value=f"To toggle the pineappleboard on and off, use {ctx.prefix}pineappleboard toggle")
-      await ctx.send(embed=embed)
-  
+      await ctx.send("This command requires the manage channels permission")
+
   @commands.command()
   async def log(self, ctx, *, payload = None):
-    if payload.split(" ", 1)[0].lower() == 'channel':
-      try:
-        payload = await TextChannelConverter().convert(ctx, str((payload.split(" ", 1)[1])))
-      except:
-        await ctx.send("Invalid channel input")
+    if ctx.message.author.guild_permissions.manage_channels:
+      if payload.split(" ", 1)[0].lower() == 'channel':
+        try:
+          payload = await TextChannelConverter().convert(ctx, str((payload.split(" ", 1)[1])))
+        except:
+          await ctx.send("Invalid channel input")
+        else:
+          cfg.data['logchannel'] = payload.id
+          dump()
+      elif payload.lower() == 'toggle':
+        if cfg.data['log'] == False:
+          cfg.data['log'] = True
+          self.bot.reload_extension('cogs.utility')
+          dump()
+        else:
+          cfg.data['log'] = False
+          cog = self.bot.get_cog('Utility')
+          self.bot.remove_listener(cog.on_raw_message_delete)
+          self.bot.remove_listener(cog.on_member_ban)
+          dump()
+      elif payload.split(" ", 1)[0].lower() == 'deletedmessages':
+        payload = payload.split(" ", 1)[1]
+        try:
+          payload = await TextChannelConverter().convert(ctx, str(payload))
+        except:
+          await ctx.send("Invalid channel input")
+        else:
+          cfg.data['deletedmessageschannel'] = payload.id
+          dump()
       else:
-        cfg.data['logchannel'] = payload.id
-        dump()
-    elif payload.lower() == 'toggle':
-      if cfg.data['log'] == False:
-         cfg.data['log'] = True
-         self.bot.reload_extension('cogs.utility')
-         dump()
-      else:
-         cfg.data['log'] = False
-         cog = self.bot.get_cog('Utility')
-         self.bot.remove_listener(cog.on_raw_message_delete)
-         self.bot.remove_listener(cog.on_member_ban)
-         dump()
-    elif payload.split(" ", 1)[0].lower() == 'deletedmessages':
-      payload = payload.split(" ", 1)[1]
-      try:
-        payload = await TextChannelConverter().convert(ctx, str(payload))
-      except:
-        await ctx.send("Invalid channel input")
-      else:
-        cfg.data['deletedmessageschannel'] = payload.id
-        dump()
+        embed = cfg.buildembed("Log Settings", "Here's a list of log settings")
+        embed.add_field(name="Channel", value=f"To change the channel used for logging, use {ctx.prefix}log channel [channel]")
+        embed.add_field(name="Toggle", value=f"To toggle logging on and off, use {ctx.prefix}log toggle")
+        embed.add_field(name="Deleted Messages", value=f"To change the deleted messages channel, use {ctx.prefix}log deletedmessages [channel]")
+        await ctx.send(embed=embed)
     else:
-      embed = cfg.buildembed("Log Settings", "Here's a list of log settings")
-      embed.add_field(name="Channel", value=f"To change the channel used for logging, use {ctx.prefix}log channel [channel]")
-      embed.add_field(name="Toggle", value=f"To toggle logging on and off, use {ctx.prefix}log toggle")
-      embed.add_field(name="Deleted Messages", value=f"To change the deleted messages channel, use {ctx.prefix}log deletedmessages [channel]")
-      await ctx.send(embed=embed)
-
-        
-
-  
+      await ctx.send("This command requires the manage channels permission")
 
 def setup(bot):
   bot.add_cog(Settings(bot))
