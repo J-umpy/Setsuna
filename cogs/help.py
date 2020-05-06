9b53585c28fc0909c94af29acde052fb25d3f92b
Jumpyvonvagabond <36978560+Jumpyvonvagabond@users.noreply.github.com>
2020-05-05 17:58:24 -0700

    Optimizations

--- cogs/help.py
+++ cogs/help.py
@@ -9,35 +9,59 @@ class Help(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self._last_member = None
-  @commands.command(aliases=['h'])
-  async def help(self, ctx, payload = "ersjioghsouighouis"):
-    async def buildhelp(ctx, payload):
-      if commands.Bot.get_command(self.bot, payload) == None:
-        if self.bot.get_cog(payload.title()) == None:
-          embed = cfg.buildembed("Welcome to the Help Center!", "Here is a list of modules")
-          embed.add_field(name="For more info use", value=f"{ctx.prefix}help [module name]")
-          embed.set_footer(text="Bot by Jumpy♡#0150")
-          for cog in cfg.cogs:
-            embed.add_field(name=cog.replace('cogs.', '').title(), value=f"A list of commands in the {cog.replace('cogs.', '').title()} Module", inline=False)
-        else:
-          cog = self.bot.get_cog(payload.title())
-          embed = cfg.buildembed(f"List of {payload.title()} Commands", f"For more information use {ctx.prefix}help [command name]")
-          for cmd in cog.get_commands():
-            embed.add_field(name=cmd.name, value=data[payload.lower()][cmd.name]['description'], inline=False)
+  
+  async def buildhelp(self, ctx, payload, page):
+    if commands.Bot.get_command(self.bot, payload) == None:
+      if self.bot.get_cog(payload.title()) == None:
+        embed = cfg.buildembed("Welcome to the Help Center!", "Here is a list of modules")
+        embed.add_field(name="For more info use", value=f"{ctx.prefix}help [module name]")
+        embed.set_footer(text="Bot by Jumpy♡#0150")
+        for cog in cfg.cogs:
+          embed.add_field(name=cog.replace('cogs.', '').title(), value=f"A list of commands in the {cog.replace('cogs.', '').title()} Module", inline=False)
       else:
-        command = commands.Bot.get_command(self.bot, payload)
-        embed = cfg.buildembed(command.name.title(), data[command.cog.qualified_name.lower()][command.name]['description'])
-        embed.add_field(name='Usage', value=f"{ctx.prefix}{data[command.cog.qualified_name.lower()][command.name]['usage']}", inline=False)
-        embed.add_field(name='Info', value=data[command.cog.qualified_name.lower()][command.name]['info'], inline=False)
+        cog = self.bot.get_cog(payload.title())
+        embed = cfg.buildembed(f"List of {payload.title()} Commands", f"For more information use {ctx.prefix}help [command name]")
+        count = 0
+        try:
+          int(page)
+          floor = (page*10) - 10
+        except:
+          page = 1
+          floor = 0
+        if page < 1:
+          page = 1
+        commandlist = cog.get_commands()
+        total = len(commandlist)
+        if total < 10:
+          total = 10
+        commandlist = commandlist[floor:]
+        for cmd in commandlist:
+          if count < 10:
+            embed.add_field(name=cmd.name, value=data[payload.lower()][cmd.name]['description'], inline=False)
+            count += 1
+          else:
+            break
+        embed.set_footer(text=f"Page {page} of {total//10}")
+    else:
+      command = commands.Bot.get_command(self.bot, payload)
+      embed = cfg.buildembed(command.name.title(), data[command.cog.qualified_name.lower()][command.name]['description'])
+      embed.add_field(name='Usage', value=f"{ctx.prefix}{data[command.cog.qualified_name.lower()][command.name]['usage']}", inline=False)
+      embed.add_field(name='Info', value=data[command.cog.qualified_name.lower()][command.name]['info'], inline=False)
+      if len(command.aliases) > 0:
         if len(command.aliases) > 1:
-          aliaslist = ""
+          aliaslist = ''
           for alias in command.aliases:
             aliaslist += f"{alias}, "
         else:
           aliaslist = command.aliases[0]
-        embed.add_field(name='Aliases', value=aliaslist, inline=False)
-      return embed
-    embed = await buildhelp(ctx, payload)
+      else:
+        aliaslist = 'None'
+      embed.add_field(name='Aliases', value=aliaslist, inline=False)
+    return embed
+    
+  @commands.command(aliases=['h'])
+  async def help(self, ctx, payload = 'None', page = None):
+    embed = await Help.buildhelp(self, ctx, payload, page)
     await ctx.send(embed=embed)
 def setup(bot):
   bot.add_cog(Help(bot))
--- cogs/utility.py
+++ cogs/utility.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import MemberConverter
-from discord.ext.commands import EmojiConverter
-from discord.ext.commands import RoleConverter
+from discord.ext.commands import bot, MemberConverter, TextChannelConverter, EmojiConverter, PartialEmojiConverter, RoleConverter
 import json
 import asyncio
 import cfg
@@ -14,7 +12,8 @@ class Utility(commands.Cog):
   @commands.command()
   async def invite(self, ctx):
     if 'VANITY_URL' in ctx.guild.features:
-      await ctx.send(ctx.guild.vanity_invite())
+      invites = await ctx.guild.vanity_invite()
+      await ctx.send(invites.url)
     else:
       invites = await ctx.guild.invites()
       await ctx.send(invites[0])
@@ -68,36 +67,31 @@ class Utility(commands.Cog):
     if floor < 0:
       floor = 0
     bans =  await ctx.guild.bans()
+    total = len(bans)
+    bans = bans[floor:]
     counter = 0
-    while counter < 10:
-        embed.add_field(name=bans[floor].user, value=bans[floor].reason, inline=False)
+    for ban in bans:
+      if counter < 10:
+        embed.add_field(name=ban.user, value=ban.reason, inline=False)
         counter += 1
-        floor += 1
-    embed.set_footer(text=f'Page {page} of {len(bans)//10}')
+      else:
+        break
+    embed.set_footer(text=f'Page {page} of {total//10}')
     await ctx.send(embed=embed)
 
   @commands.command(aliases=['se', 'showemoji', 'stealemoji', 'stealemote', 'viewemote', 'viewemoji'])
   async def showemote(self, ctx, emote = None):
     if emote == None:
-      await ctx.send("You didn't specify an emote to show")
-    try:
-      emote = await EmojiConverter().convert(ctx, emote)
-    except:
-      try:
-        await PartialEmojiConverter.convert(ctx, emote)
-      except:
-        cfg.buildembed('Show Emote', "I couldn't find the emote you were looking for")
-      else:
-        emote = emote.replace('<', '')
-        emote = emote.replace('>', '')
-        emote = emote.rsplit(':', 2)
-        embed = cfg.buildembed(emote[1], f"ID: {emote[2]}")
-        embed.set_image(url=f"https://cdn.discordapp.com/emojis/{emote[2]}.png")
-        await ctx.send(embed=embed)
+      embed = cfg.buildembed("Show Emote", "You didn't specify an emote for me to find")
     else:
-      embed = cfg.buildembed(emote.name, f"ID: {emote.id}")
-      embed.set_image(url=emote.url)
-      await ctx.send(embed=embed)
+      try:
+        emote = await PartialEmojiConverter().convert(ctx, emote)
+      except:
+        embed = cfg.buildembed('Show Emote', "I couldn't find the emote you were looking for")
+      else:
+        embed = cfg.buildembed(emote.name, f"ID: {emote.id}")
+        embed.set_image(url=emote.url)
+    await ctx.send(embed=embed)
   
   @commands.command(aliases=['ui', 'userinformation', 'whois'])
   async def userinfo(self, ctx, member = None):
@@ -140,6 +134,45 @@ class Utility(commands.Cog):
         embed = cfg.buildembed('Member Count', f'There are {role} members with the {role.name} role in {ctx.guild.name}')
         await ctx.send(embed=embed)
   
+  @commands.command()
+  async def poll(self, ctx, channel = None):
+    try:
+      channel = await TextChannelConverter().convert(ctx, channel)
+    except:
+      channel = ctx.channel
+    def check(m):
+      return m.channel == ctx.channel and m.author == ctx.author
+    embed = cfg.buildembed('Poll', 'Enter a poll question')
+    await ctx.send(embed=embed)
+    try:
+      desc = await self.bot.wait_for('message', timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+      embed = cfg.buildembed('Poll', "I got tired of waiting for you to say something. Try again later, alright?")
+      await ctx.send(embed=embed)
+    else:
+      count = 0
+      embed = cfg.buildembed('Poll', 'Enter up to 5 poll options. Say `done` when finished')
+      await ctx.send(embed=embed)
+      embed = cfg.buildembed('Poll', desc.content)
+      while count < 5:
+        try:
+          option = await self.bot.wait_for('message', timeout=60.0, check=check)
+          if option.content.lower() == 'done':
+            break
+          key = ['\N{REGIONAL INDICATOR SYMBOL LETTER A}', '\N{REGIONAL INDICATOR SYMBOL LETTER B}', '\N{REGIONAL INDICATOR SYMBOL LETTER C}', '\N{REGIONAL INDICATOR SYMBOL LETTER D}', '\N{REGIONAL INDICATOR SYMBOL LETTER E}']
+          embed.add_field(name='\u200b', value=f'{key[count]} {option.content}', inline=False)
+          await option.delete(delay=1.0)
+          count += 1
+        except asyncio.TimeoutError:
+          embed = cfg.buildembed('Poll', "I got tired of waiting for you to say something\nTry again when you've made up your mind, okay?")
+      await channel.send(embed=embed)
+      if embed.fields != discord.Embed.Empty:
+        message = await channel.fetch_message(channel.last_message_id)
+        count = -1
+        for i in embed.fields:
+          count += 1
+          await message.add_reaction(key[count])
+
   @commands.Cog.listener()
   async def on_raw_message_delete(self, deleted_message):
     if deleted_message.cached_message == None:
@@ -163,51 +196,57 @@ class Utility(commands.Cog):
       
   @commands.Cog.listener()
   async def on_member_ban(self, guild, user):
-    ban = await guild.fetch_ban(user)
-    embed = cfg.buildembed("Member banned", f"{str(user)} was banned from {guild}", discord.Colour.red())
-    embed.add_field(name="Reason", value=ban.reason)
-    channel = self.bot.get_channel(cfg.data["logchannel"])
-    await channel.send(embed=embed)
+    if cfg.data['log'] == True:
+      ban = await guild.fetch_ban(user)
+      embed = cfg.buildembed("Member banned", f"{str(user)} was banned from {guild}", discord.Colour.red())
+      embed.add_field(name="Reason", value=ban.reason)
+      channel = self.bot.get_channel(cfg.data["logchannel"])
+      await channel.send(embed=embed)
 
   @commands.Cog.listener()
   async def on_member_join(self, member):
-    channel = self.bot.get_channel(cfg.data["welcomechannel"])
-    key = {'MENTION': member.mention, 'SERVER': member.guild.name}
-    await channel.send(cfg.data["welcome"]["message"].format(**key))
+    if cfg.data['welcome']['enabled'] == True:
+      channel = self.bot.get_channel(cfg.data["welcomechannel"])
+      key = {'MENTION': member.mention, 'SERVER': member.guild.name}
+      await channel.send(cfg.data["welcome"]["message"].format(**key))
 
   @commands.Cog.listener()
   async def on_raw_reaction_add(self, payload):
-    if payload.emoji.name == '\N{PINEAPPLE}':
-      channel = self.bot.get_channel(cfg.data["pineappleboard"]["channel"])
-      rchannel= self.bot.get_channel(payload.channel_id)
-      rmessage = await rchannel.fetch_message(payload.message_id)
-      for reaction in rmessage.reactions:
-        if reaction.emoji == '\N{PINEAPPLE}':
-          reaction = reaction
-          break
-      if reaction.count > cfg.data['pineappleboard']['count']:
-        async for m in channel.history(limit=5):
-          if int(m.embeds[0].fields[0].value[82:].replace(")", "")) == reaction.message.id:
-            for r in reaction.message.reactions:
-              if r.emoji == '\N{PINEAPPLE}':
-                count = r.count
-              break
-            embed = m.embeds[0]
-            embed.set_footer(text=f'Highest {reaction.emoji}: {count}')
-            await m.edit(embed=embed)
+    if cfg.data['pineappleboard']['enabled'] == True:
+      if payload.emoji.name == '\N{PINEAPPLE}':
+        channel = self.bot.get_channel(cfg.data["pineappleboard"]["channel"])
+        rchannel= self.bot.get_channel(payload.channel_id)
+        rmessage = await rchannel.fetch_message(payload.message_id)
+        for reaction in rmessage.reactions:
+          if reaction.emoji == '\N{PINEAPPLE}':
+            reaction = reaction
             break
-      elif reaction.count == cfg.data['pineappleboard']['count']:
-        if reaction.message.channel != channel: 
-          cfg.buildembed(str(reaction.message.author), reaction.message.content, discord.Colour.gold())
-          embed.add_field(name="Original Message", value=f"[Jump Link]({reaction.message.jump_url})", inline=False)
-          embed.set_thumbnail(url=reaction.message.author.avatar_url)
-          embed.set_footer(text=f'Highest {reaction.emoji}: {reaction.count}')
-          att = rmessage.attachments[0]
-          if att.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
-            embed.set_image(url=att.url)
-          else:
-            embed.add_field(name='Message Attachment', value=f'[{att.filename}]({att.url})', inline=False)
-          await channel.send(embed=embed)
+        if reaction.count > cfg.data['pineappleboard']['count']:
+          def predicate(msg):
+            return msg.author.bot
+          async for m in channel.history(limit=5).filter(predicate):
+            if int(m.embeds[0].fields[0].value[82:].replace(")", "")) == reaction.message.id:
+              for r in reaction.message.reactions:
+                if r.emoji == '\N{PINEAPPLE}':
+                  count = r.count
+                break
+              embed = m.embeds[0]
+              embed.set_footer(text=f'Highest\N{PINEAPPLE}: {count}')
+              await m.edit(embed=embed)
+              break
+        elif reaction.count == cfg.data['pineappleboard']['count']:
+          if reaction.message.channel != channel: 
+            embed = cfg.buildembed(str(reaction.message.author), reaction.message.content, discord.Colour.gold())
+            embed.add_field(name="Original Message", value=f"[Jump Link]({reaction.message.jump_url})", inline=False)
+            embed.set_thumbnail(url=reaction.message.author.avatar_url)
+            embed.set_footer(text=f'Highest {reaction.emoji}: {reaction.count}')
+            att = rmessage.attachments
+            if len(att) != 0:
+              if att[0].url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
+                  embed.set_image(url=att[0].url)
+              else:
+                  embed.add_field(name='Message Attachment', value=f'[{att[0].filename}]({att[0].url})', inline=False)
+            await channel.send(embed=embed)
 
 def setup(bot):
   bot.add_cog(Utility(bot))
