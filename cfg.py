12fa1dd1a6f088bee705819cee00ecd9222c1567
Jumpyvonvagabond <36978560+Jumpyvonvagabond@users.noreply.github.com>
2020-05-29 13:01:12 -0700

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,6 +1,12 @@
 import json
 import discord
 cogs = ['cogs.administration', 'cogs.utility', 'cogs.help', 'cogs.settings', 'cogs.fun']
+with open('config.json') as f:
+  data = json.loads(f.read())
+async def reload():
+  global data
+  with open('config.json') as f:
+    data = json.loads(f.read())
 def buildembed(title = "Placeholder Title", description = "Placeholder Description", colour = discord.Colour.from_rgb(251, 217, 229)):
   embed = discord.Embed(title=title, description=description, colour=colour)
   return embed
