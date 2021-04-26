5b2ee12e4120c0f7584fc39e07a3117db71a7666
J-umpy <36978560+J-umpy@users.noreply.github.com>
2021-04-26 15:40:28 -0700

    literally fixed a typo

--- launcher.py
+++ launcher.py
@@ -8,7 +8,7 @@ import tools
 #Required as of Version 1.5 to tell Discord what events to subscribe to, default() is all
 intents = discord.Intents.default()
 intents.members = True
-intents.presence = True
+intents.presences = True
 
 #Grabs prefixes from the list in config.json and allows Setsuna to respond to commands when pinged
 def get_prefix(client, message):
